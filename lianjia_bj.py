994c1f08e0de74950f32cd237cad845a8e0b27e2
lihongjie0209 <lihongjie0209@gmail.com>
2017-02-17 00:11:29 +0800

    first commit

--- lianjia_bj.py
+++ lianjia_bj.py
@@ -1,11 +1,11 @@
 # -*- coding: utf-8 -*-
 import scrapy
+from fake_useragent import UserAgent
 from scrapy.linkextractors import LinkExtractor
 from scrapy.loader import ItemLoader
-from scrapy.loader.processors import TakeFirst
-from scrapy.spiders import CrawlSpider, Rule
-from fake_useragent import UserAgent
-from scrapy.settings import default_settings
+from scrapy.loader.processors import Join
+from scrapy.spiders import CrawlSpider
+from scrapy.spiders import Rule
 
 
 class LianjiaItem(scrapy.Item):
@@ -28,45 +28,43 @@ class LianjiaSpider(CrawlSpider):
     name = 'lianjia'
     download_delay = 3.0
     ua = UserAgent()
-    custom_settings = {'USER_AGENT': ua.random, 'HTTPCACHE_ENABLED': True, 'COOKIES_ENABLED': False}
+    # 调试时请使用缓存
+    # custom_settings = {'HTTPCACHE_ENABLED': True}
     start_urls = ['http://bj.lianjia.com/ershoufang/']
 
-    def parse(self, response):
-        for area in response.xpath('//div[@data-role="ershoufang"]//a/@href'):
-            rel_link = area.extract()
-            yield scrapy.Request(url=response.urljoin(rel_link), callback=self.next_page,
-                                 headers={'USER_AGENT': self.ua.random})
+    rules = (
+        Rule(LinkExtractor(restrict_xpaths=['//div[@data-role="ershoufang"]//a']), callback='next_page'),
+    )
 
     def next_page(self, response):
 
         page_url = response.xpath('//@page-url').extract_first()
         page_data = response.xpath('//@page-data').extract_first()
-        # total_page = eval(page_data)['totalPage']
-        total_page = 2
+        total_page = eval(page_data)['totalPage']
+        # total_page = 2
         for page in range(1, total_page + 1):
             rel_url = page_url.format(page=page)
             yield scrapy.Request(url=response.urljoin(rel_url), callback=self.parse_item,
-                                 headers={'USER_AGENT': self.ua.random})
+                                 headers={'User-Agent': self.ua.random})
 
     def parse_item(self, response):
-        for house in response.xpath('//ul[@sellListContent]/li'):
+        for house in response.xpath('//ul[@class="sellListContent"]/li'):
             l = ItemLoader(item=LianjiaItem(), response=response, selector=house)
-            l.default_output_processor = TakeFirst()
+            l.default_output_processor = Join()
             l.add_xpath('title', './/div[@class="title"]/a/text()')
-            l.add_xpath('community', './/div[class="houseInfo""]/a/text()')
+            l.add_xpath('community', './/div[@class="houseInfo"]/a/text()')
             l.add_xpath('model', './/div[@class="houseInfo"]/text()', re=r'\d室\d厅')
             l.add_xpath('area', './/div[@class="houseInfo"]/text()', re=r'[\d\.]+平米')
-            l.add_xpath('direction', './/div[@class="houseInfo"]/text()', re=r'[东西南北 ]+')
+            l.add_xpath('direction', './/div[@class="houseInfo"]/text()', re=r'[东西南北]+')
             l.add_xpath('decoration', './/div[@class="houseInfo"]/text()', re=r'.装')
             l.add_xpath('focus_num', './/div[@class="followInfo"]/text()', re=r'\d+人关注')
             l.add_xpath('watch_num', './/div[@class="followInfo"]/text()', re=r'共\d+次带看')
-            l.add_xpath('time', './/div[@class="followInfo"]/text()', re=r'[\W]+以前发布')
+            l.add_xpath('time', './/div[@class="followInfo"]/text()', re=r'\d+[天月个]以前发布')
             l.add_xpath('price', './/div[@class="totalPrice"]/span/text()')
             l.add_xpath('average_price', './/div[@class="unitPrice"]/@data-price')
             l.add_xpath('link', './/div[@class="title"]/a/@href')
             item = l.load_item()
             yield item
-            print(item)
 
 
 if __name__ == '__main__':
